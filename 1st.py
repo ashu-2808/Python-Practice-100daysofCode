a373e0aad542a668b85e355b2c1d44b73a595c83
Ashutosh Kumar <119068474+ashu-2808@users.noreply.github.com>
2023-04-02 01:56:09 +0530

    initial commit

--- 1st.py
+++ 1st.py
@@ -1,1 +1,7 @@
-print("Hello I am Ashutosh Kumar ")
+print("Hello I am \"Ashutosh Kumar\"",end=":-\n")
+print("Btech(CSE) student from Lovely Profession University","2nd year",sep="~")
+#ABOUT
+'''As a student of computer science and engineering, I am fascinated by technology ,innovation and  Data Science enthusiast. I am continuously investigating new technologies and programming languages to increase my knowledge and ability.
+Throughout my academic career, I have also refined my abilities in programming languages such as Java, Python,SQL, and C++, allowing me to provide efficient and effective solutions to challenging challenges.
+My ultimate goal is to use my technical knowledge and passion for innovation to make a difference in the computer science and engineering industries
+'''
